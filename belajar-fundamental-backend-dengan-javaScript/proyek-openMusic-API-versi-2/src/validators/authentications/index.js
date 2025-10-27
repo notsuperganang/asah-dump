a3b1e95c9e2d88aaac83380e0a6b42172cb24b3d
notsuperganang <ganangsetyohadi@gmail.com>
2025-10-27 11:29:19 +0700

    feat: implement Joi validation schemas for all endpoints

---
+++ belajar-fundamental-backend-dengan-javaScript/proyek-openMusic-API-versi-2/src/validators/authentications/index.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const PostAuthenticationPayloadSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+const PutAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required(),
+});
+
+const DeleteAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required(),
+});
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
---
+++ belajar-fundamental-backend-dengan-javaScript/proyek-openMusic-API-versi-2/src/validators/collaborations/index.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const CollaborationPayloadSchema = Joi.object({
+  playlistId: Joi.string().required(),
+  userId: Joi.string().required(),
+});
+
+const CollaborationValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = CollaborationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationValidator;
